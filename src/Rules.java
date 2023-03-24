94cb0e2c5acab7c1950ac9e7fe99b01e258ae990
dev <dev@gmail.com>
2023-03-24 19:08:26 +0530

    git

---
+++ src/Rules.java
@@ -0,0 +1,12 @@
+public class Rules
+{
+    public static void main(String[] args) {
+        int y=5;
+        for (int i=1;i<=100;i++){
+            for (int o=1;o<=i;o++){
+                System.out.print("sorry");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Sub.java
@@ -0,0 +1,8 @@
+public class Sub {
+    public static void main(String[] args) {
+        int a=99;
+        int v=77;
+        int y=a-v;
+        System.out.println(y);
+    }
+}
